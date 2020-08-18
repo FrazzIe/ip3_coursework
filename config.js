b0c1c2ecc0b375d6404e0973ea76593f16e86346
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-18 18:38:30 +0100

    add empty config, load in env vars

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv').config(); //load .env file into process.env.*
+
+if (dotenv.error) { //throw an error if the environment vars failed to load
+    throw dotenv.error
+}
+
+const config = {
+
+}
+
+module.exports = config;
