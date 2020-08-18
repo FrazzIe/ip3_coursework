fc3be75e99acfb2a948e065384ea3926b5f1b891
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-18 19:06:01 +0100

    setup mustache and directories

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const config = require("./config");
 const express = require("express");
+const mustache = require("mustache-express");
 
 var app = express(); //init express app
 
+app.engine("mustache", mustache(__dirname + "/public/views/partials"));
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/public/views"); //setup mustache
+
 app.listen(config.app.port, () => { //make app listen for port
 	console.log("quiz_manager listening on port " + config.app.port);
-})
+});
