0f3dea4b6eb3f1ee3fd681f1d042cf9462b68816
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-18 19:42:56 +0100

    set up sessions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require("./config");
 const mysql = require("./models/mysql");
 const express = require("express");
 const mustache = require("mustache-express");
+const session = require("express-session");
 
 var app = express(); //init express app
 
@@ -9,6 +10,9 @@ app.use(express.static(__dirname + "/public")); //serve static files
 app.engine("mustache", mustache(__dirname + "/public/views/partials"));
 app.set("view engine", "mustache");
 app.set("views", __dirname + "/public/views"); //setup mustache
+app.use(session({
+	secret: config.session.secret,
+})); //setup session
 
 app.listen(config.app.port, () => { //make app listen for port
 	console.log("quiz_manager listening on port " + config.app.port);
--- config.js
+++ config.js
@@ -14,6 +14,9 @@ const config = {
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASS,
         database: process.env.MYSQL_DB,
+    },
+    session: {
+        secret: process.env.SESSION_SECRET
     }
 }
 
