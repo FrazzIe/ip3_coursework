4af2c29ed561e8cd29d9062fb82a729153ec8d7e
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-18 18:44:58 +0100

    add express, init express app

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const config = require("./config");
+const express = require("express");
+
+var app = express(); //init express app
 
 app.listen(config.app.port, () => { //make app listen for port
 	console.log("Coursework scheduler listening on port " + config.app.port);
