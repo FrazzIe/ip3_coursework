f55824c90d4042f62f67d940d170c43b1e1d0350
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-18 18:41:57 +0100

    load config, use config port instead

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-app.listen(process.env.PORT || 3000, () => { //make app listen for port
-	console.log("Coursework scheduler listening on port " + (process.env.PORT || 3000));
+const config = require("./config");
+
+app.listen(config.app.port, () => { //make app listen for port
+	console.log("Coursework scheduler listening on port " + config.app.port);
 })
