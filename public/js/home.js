dedffaf5cc0a8a8ffab6f18251d99680e24da1f1
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-22 04:26:25 +0100

    fix indentation

--- public/js/home.js
+++ public/js/home.js
@@ -1,27 +1,27 @@
 function getQuizEstimation(count, num) {
-    if (count == 0)
-        return "minute or so";
-    
-    if (num > 60)
-        num = 60;
+	if (count == 0)
+		return "minute or so";
+	
+	if (num > 60)
+		num = 60;
 
-    let estimate = count * num;
+	let estimate = count * num;
 
-    return estimate + " minute" + (estimate == 1 ? "" : "s");
+	return estimate + " minute" + (estimate == 1 ? "" : "s");
 }
 
 function setQuizEstimations() {
-    let elements = document.querySelectorAll("span[id^='estimation-']");
-    elements.forEach(function(element) {
-        element.textContent = getQuizEstimation(element.dataset.count, 2);
-    });
+	let elements = document.querySelectorAll("span[id^='estimation-']");
+	elements.forEach(function(element) {
+		element.textContent = getQuizEstimation(element.dataset.count, 2);
+	});
 }
 
 function setQuizPercentages() {
-    let elements = document.querySelectorAll("span[id^='percentage-']");
-    elements.forEach(function(element) {
-        element.textContent = (element.dataset.score / element.dataset.count * 100) + "%";
-    });
+	let elements = document.querySelectorAll("span[id^='percentage-']");
+	elements.forEach(function(element) {
+		element.textContent = (element.dataset.score / element.dataset.count * 100) + "%";
+	});
 }
 
 setQuizEstimations();
