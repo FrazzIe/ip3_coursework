88f722d525dd71fb860cd003469b40e85b57b4cd
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-22 04:33:17 +0100

    send quiz label to server

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,5 @@
+const createForm = document.getElementById("create-form");
+
 function getQuizEstimation(count, num) {
 	if (count == 0)
 		return "minute or so";
@@ -26,3 +28,23 @@ function setQuizPercentages() {
 
 setQuizEstimations();
 setQuizPercentages();
+
+createForm.addEventListener("submit", function(event) { //add a listener for when the form is submitted
+	event.preventDefault(); //prevent default form behaviour
+
+	axios.post("/create", { //make a request to the server
+		label: document.getElementById("create-label").value,
+	}).then((resp) => {
+		if (resp.data) {
+			if (typeof resp.data != "string") {
+				window.location.href = "/edit/" + resp.data;
+			} else {
+				$("#errorModalTitle").text("An error occurred");
+				$("#errorModalText").text(resp.data);
+				$("#errorModal").modal("show"); //display warning
+			}
+		}
+	}).catch((error) => {
+		console.log(error);
+	});
+});
