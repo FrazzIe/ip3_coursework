eb715c9f93b800202712a3394b9bb6198cd3ca71
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-08-23 05:22:10 +0100

    add quiz completion request

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,3 +1,5 @@
+const finishBtn = document.getElementById("finish-btn");
+
 function setFirstAnswerChecked() {
 	let elements = document.querySelectorAll("input[id^='quiz-answer-']");
 	let dupeList = {};
@@ -22,4 +24,42 @@ function getCheckedAnswers() {
 
 	return answers;
 }
+
+finishBtn.addEventListener("click", function(event) {
+	let quizElement = document.getElementById("quiz-id");
+
+	if (!quizElement.dataset || !quizElement.dataset.id || !quizElement.dataset.count) {
+		location.reload();
+		return;
+	}
+
+	let answers = getCheckedAnswers();
+
+	if (answers.length == quizElement.dataset.count) { //this shouldn't happen
+		axios.post("/quiz/finish/" + quizElement.dataset.id, { //make a request to the server
+			answers: answers,
+		}).then((resp) => {
+			if (resp.data) {
+				if (typeof resp.data != "string") {
+					location.reload();
+				} else {
+					if (resp.data.startsWith("/")) {
+						location.href = resp.data;
+					} else {
+						$("#errorModalTitle").text("An error occurred");
+						$("#errorModalText").text(resp.data);
+						$("#errorModal").modal("show"); //display warning
+					}
+				}
+			}
+		}).catch((error) => {
+			console.log(error);
+		});
+	} else {
+		$("#errorModalTitle").text("An error occurred");
+		$("#errorModalText").text("You must answer all questions!");
+		$("#errorModal").modal("show"); //display warning
+	}
+});
+
 setFirstAnswerChecked();
